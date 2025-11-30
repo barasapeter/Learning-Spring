5f4c6e9104e655a6442516a607584866bd30ab78
barasapeter <barasapeter52@gmail.com>
2025-11-30 14:12:20 +0300

    Succeeded returning file response

---
+++ learn/src/main/java/com/barasa/learn/FileController.java
@@ -0,0 +1,27 @@
+package com.barasa.learn;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+
+@RestController
+public class FileController {
+
+    @GetMapping("/file-response")
+    public ResponseEntity<Resource> downloadRootFile() {
+        File file = new File("files/me.jpg");
+        if (!file.exists()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Resource resource = new FileSystemResource(file);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=me.jpg")
+                .body(resource);
+    }
+}
